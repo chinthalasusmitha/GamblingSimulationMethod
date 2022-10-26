c9d18229737124500bee041a06daf864153d0d0b
susmitha <susmithareddychinthala024@gmail.com>
2022-10-26 21:28:31 +0530

    Added Welcome message for Gambler Simulation

---
+++ src/com/gamblingsimulation/DisplayMessage.java
@@ -0,0 +1,7 @@
+package com.gamblingsimulation;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("@------------Welcome to Gambler Simulation-------------@");
+    }
+}
